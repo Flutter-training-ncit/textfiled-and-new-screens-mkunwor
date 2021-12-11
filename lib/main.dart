6ce7e7de7959defea6f321c04938b3762c7de4f8
mkunwor <69802529+mkunwor@users.noreply.github.com>
2021-12-11 17:03:00 +0545

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:textfield_demo/home.dart';
-import 'package:flutter/cupertino.dart';
+
+import 'tiktok_navbar.dart';
+
 
 void main() {
-  runApp(MaterialApp(home: SignupPage()));
+  runApp(const MyApp());
 }
 
-class SignupPage extends StatefulWidget {
-  const SignupPage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  _SignupPageState createState() => _SignupPageState();
-}
-
-class _SignupPageState extends State<SignupPage> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Facebook',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-        backgroundColor: Colors.purple[700],
-        elevation: 0,
-        leading:
- Icon(
-            Icons.facebook_sharp,
-            color: Colors.white,
-            size: 40,
-       
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                ),
-              ),
-            ),
-            ElevatedButton(onPressed: (){
-
-       Navigator.push(context,
-  CupertinoPageRoute (builder:(context){
-   return homepage();
- }),
- );
-
-
-            },
-             style: ElevatedButton.styleFrom(
-              primary: Colors.purple,
-           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          //  enabledMouseCursor: MouseCursor(
-             
-
-          //  ),
-           
-
-             ),
-            
-
-             child: Text('Sign Up'),
-            
-            )
-
-          ],
-        ),
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return TiktokNavbar();
   }
 }
